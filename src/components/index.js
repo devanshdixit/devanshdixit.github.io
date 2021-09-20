7308f909ded60ae2b56de7a26a8e961348b6da7a
dev <devanshdixit01@gmail.com>
2021-09-20 19:34:51 +0530

    updated name and links

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './Header';
-export * from './Navbar';
-export * from './Title';
-export * from './Alert';
--- src/pages/contact/base.js
+++
@@ -1,5 +0,0 @@
-const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
-const USER_ID = process.env.REACT_APP_USER_ID;
-const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
-
-export { SERVICE_ID, USER_ID, TEMPLATE_ID };
